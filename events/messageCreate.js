993bb5ccbdb881c20bdace8b3304ec0dc988e758
Jason <123jasont@gmail.com>
2022-03-13 17:35:42 -0700

    Event and command handlers

---
+++ events/messageCreate.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "messageCreate",
+    run:  async function runAll(bot, message) {
+        const {client, prefix, owners} = bot
+        if(!message.guild || message.author.bot || !message.content.startsWith(prefix)) return
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/g)
+        const cmdstr = args.shift().toLowerCase()
+
+        let command = client.commands.get(cmdstr)
+        if(!command) return
+
+        let member = message.member
+
+        if(command.devOnly && !owners.includes(member.id)) {
+            return message.reply("This command is admin-only!")
+        }
+
+        if(command.permissions && member.permissions.missing(command.permissions).length !== 0) {
+            return message.reply("You do not have permission to use this command!")
+        }
+
+        try {
+            await command.run({...bot, message, args})
+        } catch(e) {
+            let errMsg = err.toString()
+
+            if(errMsg.startsWith("?")) {
+                errMsg = errMsg.slice(1)
+                await message.reply(errMsg)
+            } else {
+                console.error(err)
+            }
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const welcomeChannelId = process.env.MESSAGE_CHANNEL_ID
-const { MessageEmbed } = require("discord.js");
-
+// const welcomeChannelId = process.env.MESSAGE_CHANNEL_ID
+// const { MessageEmbed } = require("discord.js");
 
 const client = new Discord.Client({
     intents: [
@@ -13,30 +12,47 @@ const client = new Discord.Client({
     ]
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-})
+let bot = {
+    client,
+    prefix: "!",
+    owners: ["268178914982100994"]
+}
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
-    }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-client.on("guildMemberAdd", async (member) => {
-    const welcomeEmbed = new MessageEmbed()
-	.setColor("#000000")
-	.setTitle(`Welcome to the server, ${member.user.username}#${member.user.discriminator}!`)
-	.setDescription(`We hope you enjoy your stay! Make sure you check out the rules at ${member.guild.channels.cache.get('952710064307859476').toString()}.`)
-	.setThumbnail(member.displayAvatarURL())
-	.setImage("https://files.catbox.moe/qaby2m.jpg")
-	.setTimestamp()
-	.setFooter({ text: "botscribe v0.0.1", iconURL: "https://files.catbox.moe/ee352a.jpg" });
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
 
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}>`,
-        embeds: [welcomeEmbed]
-    })
-})
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}!`)
+// })
+
+// client.on("messageCreate", (message) => {
+//     if (message.content == "hi"){
+//         message.reply("Hello World!")
+//     }
+// })
+
+// client.on("guildMemberAdd", async (member) => {
+//     const welcomeEmbed = new MessageEmbed()
+// 	.setColor("#000000")
+// 	.setTitle(`Welcome to the server, ${member.user.username}#${member.user.discriminator}!`)
+// 	.setDescription(`We hope you enjoy your stay! Make sure you check out the rules at ${member.guild.channels.cache.get('952710064307859476').toString()}.`)
+// 	.setThumbnail(member.displayAvatarURL())
+// 	.setImage("https://files.catbox.moe/qaby2m.jpg")
+// 	.setTimestamp()
+// 	.setFooter({ text: "botscribe v0.0.1", iconURL: "https://files.catbox.moe/ee352a.jpg" });
+
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}>`,
+//         embeds: [welcomeEmbed]
+//     })
+// })
 
 client.login(process.env.TOKEN)
